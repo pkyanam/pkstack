c4ae4f5a24bea1fa746e70044735166542c79876
Preetham Kyanam <kyanam.preetham@gmail.com>
2026-04-08 17:57:34 -0400

    fix: replace next lint with eslint CLI in template, add eslint configs

---
+++ packages/cli/templates/web/eslint.config.js
@@ -0,0 +1,12 @@
+import { baseConfig } from '@pkstack/config/eslint'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(...baseConfig, {
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  ignores: ['dist/', '.next/'],
+})
